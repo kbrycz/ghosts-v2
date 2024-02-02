84cf7f22a4633cdae7985cb10fde44e6057c76e3
Karl Brycz <kbrycz@umich.edu>
2024-02-01 21:36:27 -0600

    change server

--- global/Global.js
+++ global/Global.js
@@ -1,5 +1,4 @@
-// export let server = "http://3.128.106.88:3000"
+export let server = "http://52.15.212.84:3000"
 // export let server = "http://f0ee-168-91-239-128.ngrok.io"
-export let server = "https://nodejs-olive-chi.vercel.app:3000"
 // export let server = "http://10.0.0.207:3000"
 export let socket = null
