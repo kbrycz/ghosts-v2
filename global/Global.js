c83bac873ed79b99bb1ca74d33e355aa7586ecd3
Karl Brycz <kbrycz@umich.edu>
2024-02-01 23:35:22 -0600

    adding new server

--- global/Global.js
+++ global/Global.js
@@ -1,4 +1,4 @@
-export let server = "http://52.15.212.84:3000"
+export let server = "https://www.ghosts-server.online"
 // export let server = "http://f0ee-168-91-239-128.ngrok.io"
 // export let server = "http://10.0.0.207:3000"
 export let socket = null
